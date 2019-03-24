d524366957f58da929dc922c151193046a350710
CollinPereira <collinp26@gmail.com>
2019-03-25 00:37:51 +0530

    product display

--- shoes/urls.py
+++ shoes/urls.py
@@ -4,5 +4,6 @@ from shoes import views
 urlpatterns=[
     path('nike/',views.nike,name='nike'),
     path('adidas/',views.adidas,name='adidas'),
-    path('details/',views.details,name='details'),
+    path('puma/',views.puma,name='puma'),
+    path('<int:shoe_id>/',views.details,name='details'),
 ]
--- shoes/views.py
+++ shoes/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from .models import Shoes
+from django.shortcuts import render,get_object_or_404
+
 # Create your views here.
 def home(request):
     shoes=Shoes.objects.all().order_by('-date')[:9]
@@ -12,6 +14,11 @@ def nike(request):
 def adidas(request):
     shoes=Shoes.objects.filter(brand__iexact="adidas")
     return render(request,'shoes/brand.html',{'shoes':shoes,'brand':'Adidas'})
+    
+def puma(request):
+    shoes=Shoes.objects.filter(brand__iexact="puma")
+    return render(request,'shoes/brand.html',{'shoes':shoes,'brand':'Puma'})
 
-def details(request):
-    return render(request,'shoes/details.html',{})
+def details(request,shoe_id):
+    shoe=get_object_or_404(Shoes,pk=shoe_id)
+    return render(request,'shoes/details.html',{'shoe':shoe})
